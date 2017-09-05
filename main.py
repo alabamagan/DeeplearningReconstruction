e5960549bfac9d99d02b5acac752dbdeeb32e033
lwong <fromosia@gmail.com>
2017-09-05 17:35:19 +0800

    Remove training properties settings. Train for specific slice (20-29)

--- main.py
+++ main.py
@@ -19,9 +19,7 @@ def train(net, b, trainsteps, epoch=-1):
         net.train(True)
 
     optimizer = torch.optim.SGD([{'params': net.convsModules.parameters(),
-                                  'lr': 1e-4, 'momentum':0, 'dampening':1e-4},
-                                 {'params': net.linearModules.parameters(),
-                                  'lr': 1e-3, 'momentum':0, 'dampening':1e-4},
+                                  'lr': 1e-3, 'momentum':1e-3, 'dampening':1e-4},
                                  {'params': net.miscModules.parameters(), 'lr': 1e-3},
                                  {'params': net.linear1.parameters(),
                                   'lr': 1e-5, 'momentum':0, 'dampening':1e-5}
@@ -33,20 +31,23 @@ def train(net, b, trainsteps, epoch=-1):
     ax3 = fig.add_subplot(133)
 
     criterion = torch.nn.SmoothL1Loss().cuda()
+    criterion.size_average = False
+    normalize = torch.nn.SmoothL1Loss().cuda()
+    normalize.size_average = False
     losslist = []
     for i in xrange(trainsteps):
-        sample = b[5, np.random.randint(20, 30)]
-        i2 = sample['042']
+        sample = b[np.random.randint(0, len(b)), np.random.randint(10, 20)]
+        i2 = sample['032']
         i3 = sample['064']
         gt = sample['ori']
         s = gt.shape
-        gt = Variable(torch.from_numpy(gt)).unsqueeze(0).unsqueeze(0).unsqueeze(0)
+        gt = Variable(torch.from_numpy(gt)).float().cuda()
         i2 = Variable(torch.from_numpy(i2)).unsqueeze(0).unsqueeze(0).unsqueeze(0)
         i3 = Variable(torch.from_numpy(i3)).unsqueeze(0).unsqueeze(0).unsqueeze(0)
 
         output = net.forward(i2.cuda(), i3.cuda())
         loss = criterion((output.squeeze()),
-                         (gt.squeeze().float().cuda()))
+                         (gt)) / normalize(i3.squeeze().float().cuda(), gt)
         print "[Step %03d] Loss: %.05f"%(i, loss.data[0])
         losslist.append(loss.data[0])
         loss.backward()
@@ -83,13 +84,68 @@ def train(net, b, trainsteps, epoch=-1):
 
     return losslist
 
+def evalNet(net, b, plot=True):
+    if (plot):
+        fig = plt.figure(1, figsize=[13,6])
+        ax1 = fig.add_subplot(131)
+        ax2 = fig.add_subplot(132)
+        ax3 = fig.add_subplot(133)
+
+    net.train(False)
+    criterion = torch.nn.SmoothL1Loss().cuda()
+    losslist = []
+    for i in xrange(len(b)):
+        sample = b[i]
+        i2 = sample['042']
+        i3 = sample['064']
+        gt = sample['ori']
+
+        gt = Variable(torch.from_numpy(gt)).unsqueeze(0).unsqueeze(0).unsqueeze(0)
+        i2 = Variable(torch.from_numpy(i2)).unsqueeze(0).unsqueeze(0).unsqueeze(0)
+        i3 = Variable(torch.from_numpy(i3)).unsqueeze(0).unsqueeze(0).unsqueeze(0)
+
+        output = net.forward(i2.cuda(), i3.cuda())
+        loss = criterion((output.squeeze()),
+                         (gt.squeeze().float().cuda()))
+        losslist.append(loss.data[0])
+        if (plot):
+            ax1.cla()
+            ax2.cla()
+            ax3.cla()
+            ax1.imshow(output.squeeze().cpu().data.numpy(), vmin = -1000, vmax=200, cmap="Greys_r")
+            ax2.imshow(i3.squeeze().cpu().data.numpy() - output.squeeze().cpu().data.numpy(),vmin = -5, vmax=5, cmap="jet")
+            ax3.imshow(i3.squeeze().cpu().data.numpy(),vmin = -1000, vmax=200, cmap="Greys_r")
+            plt.draw()
+            plt.pause(0.01)
+
+        # Free some meory
+        del sample, i2, i3, gt
+        gc.collect()
+
+    losslist = np.array(losslist)
+    print "======================= End Eval ======================="
+    print "average loss: ", losslist.mean()
+
 def main():
     b = BatchLoader("/media/storage/Data/CTReconstruction/LCTSC/Output")
+
+    #=========================
+    # Train
+    #---------------------
     net = network.Net()
     net.cuda()
-    l = train(net, b, trainsteps=1500, epoch=6)
+    l = train(net, b, trainsteps=1500, epoch=2)
     plt.plot(l)
     plt.show()
+
+    #==================================
+    # Evaluation
+    #---------------------------
+    # netfile = "network_E003"
+    # if (os.path.isfile(netfile)):
+    #     net = torch.load(netfile)
+    #     evalNet(net, b, True)
+
     pass
 
 if __name__ == '__main__':
